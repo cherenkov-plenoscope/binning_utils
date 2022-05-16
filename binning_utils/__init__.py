dfb2be9dbfd9f74588c50792fd78b64507a73d32
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-05-16 16:42:30 +0200

    adopt finding bins from air_shower_template_generator

--- binning_utils/__init__.py
+++ binning_utils/__init__.py
@@ -42,3 +42,54 @@ def widths(bin_edges):
     bin_edges = np.array(bin_edges)
     assert len(bin_edges) >= 2, "Need at least two edges to compute a width."
     return bin_edges[1:] - bin_edges[:-1]
+
+
+def find_bin_in_edges(bin_edges, value):
+    """
+    A wrapper around numpy.digitize with over/under-flow indication.
+
+    Parameters
+    ----------
+    bin_edges : array of floats
+        The edges of the bins.
+    value : float
+        The value to be assigned to a bin.
+
+    Returns
+    -------
+    underflow-flag, bin-index, overflow-flag
+    """
+    upper_bin_edge = int(np.digitize([value], bin_edges)[0])
+    if upper_bin_edge == 0:
+        return True, 0, False
+    if upper_bin_edge == bin_edges.shape[0]:
+        return False, upper_bin_edge - 1, True
+    return False, upper_bin_edge - 1, False
+
+
+def find_bins_in_centers(bin_centers, value):
+    """
+    Compute the weighted distance to the supports of the bins.
+    """
+    underflow, lower_bin, overflow = find_bin_in_edges(
+        bin_edges=bin_centers, value=value
+    )
+
+    upper_bin = lower_bin + 1
+    if underflow:
+        lower_weight = 0.0
+    elif overflow:
+        lower_weight = 1.0
+    else:
+        dist_to_lower = value - bin_centers[lower_bin]
+        bin_range = bin_centers[upper_bin] - bin_centers[lower_bin]
+        lower_weight = 1 - dist_to_lower / bin_range
+
+    return {
+        "underflow": underflow,
+        "overflow": overflow,
+        "lower_bin": lower_bin,
+        "upper_bin": lower_bin + 1,
+        "lower_weight": lower_weight,
+        "upper_weight": 1.0 - lower_weight,
+    }
